6b64f58d40af596b5ba2eecd09955f4224ea3990
Sabrina Jones <sabrina.jones87@gmail.com>
2017-06-10 23:00:47 +0000

    added list leadr method for cavs and warriors

--- lib/ppg_leaders/cli.rb
+++ lib/ppg_leaders/cli.rb
@@ -2,7 +2,26 @@
 class PpgLeaders::CLI
 
   def call
-    puts "Points Per Game Leaders"
+    puts "Points Per Game Leaders for the Cavaliers"
+    list_leaders_cavs
+    puts "Points Per Game Leaders for the Warriors"
+    list_leaders_warriors
+  end
+
+  def list_leaders_cavs
+   puts <<-DOC
+    1. LeBron James
+    2. Kyrie Irving
+    3. Kevin Love
+   DOC
+  end
+
+  def list_leaders_warriors
+   puts <<-DOC
+    1. Kevin Durant
+    2. Stephen Curry
+    3. Klay Thompson
+   DOC
   end
 
 end
