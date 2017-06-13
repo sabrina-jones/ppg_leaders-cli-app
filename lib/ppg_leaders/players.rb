d13afb8110030227f09096d60b331a454922e908
Sabrina Jones <sabrina.jones87@gmail.com>
2017-06-13 02:46:05 +0000

    leaders method to call scrape players, added scrape_love

--- lib/ppg_leaders/players.rb
+++ lib/ppg_leaders/players.rb
@@ -1,29 +1,9 @@
 class PpgLeaders::Players
 attr_accessor :name, :jersey, :position
-  def self.leaders
-    #retun a list of all ppg leaders cavs
-    #puts <<-DOC
-    # 1. LeBron James
-    # 2. Kyrie Irving
-    # 3. Kevin Love
-  #  DOC
-    player_1 = self.new
-    player_1.name = "Lebron James"
-    player_1.jersey = "23"
-    player_1.position = "Forward"
 
-    player_2 = self.new
-    player_2.name = "Kyrie Irving"
-    player_2.jersey = "2"
-    player_2.position = "Guard"
-
-    player_3 = self.new
-    player_3.name = "Kevin Love"
-    player_3.jersey = "0"
-    player_3.position = "Forward"
-
-    [player_1, player_2, player_3]
-  end
+def self.leaders
+   self.scrape_players
+end
 
   def self.scrape_players
   #go to cavs site, find players
@@ -34,8 +14,7 @@ attr_accessor :name, :jersey, :position
 
   players << self.scrape_james
   players << self.scrape_irving
-
-
+  players << self.scrape_love
 
   players
  end
@@ -55,5 +34,12 @@ def self.scrape_irving
   position = doc.css("span.nba-player-header__position").text
 end
 
+def self.scrape_love
+  doc = Nokogiri::HTML(open("http://www.nba.com/players/kevin/love/201567"))
+  name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
+  last_name = doc.css("p.nba-player-header__last-name").text.strip
+  jersey = doc.css("span.nba-player-header__jersey-number").text
+  position = doc.css("span.nba-player-header__position").text
+end
 
 end
