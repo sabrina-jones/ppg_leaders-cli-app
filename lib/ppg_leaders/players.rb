ff768e5456b507fafc5bb68e8a5e2384a8af0917
Sabrina Jones <sabrina.jones87@gmail.com>
2017-06-13 03:26:11 +0000

    added objects to all 3 scrape methods

--- lib/ppg_leaders/players.rb
+++ lib/ppg_leaders/players.rb
@@ -5,7 +5,7 @@ def self.leaders
    self.scrape_players
 end
 
-  def self.scrape_players
+def self.scrape_players
   #go to cavs site, find players
   #get player info
   #instantiate the player
@@ -21,25 +21,31 @@ end
 
 def self.scrape_james
    doc = Nokogiri::HTML(open("http://www.nba.com/players/lebron/james/2544"))
-   name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
-   jersey = doc.css("span.nba-player-header__jersey-number").text
-   position = doc.css("span.nba-player-header__position").text
-   binding.pry
+   player_1 = self.new
+   player_1.name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
+   player_1.jersey = doc.css("span.nba-player-header__jersey-number").text
+   player_1.position = doc.css("span.nba-player-header__position").text
+
+   player_1
+   #binding.pry
 end
 
 def self.scrape_irving
   doc = Nokogiri::HTML(open("http://www.nba.com/players/kyrie/irving/202681"))
-  name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
-  jersey = doc.css("span.nba-player-header__jersey-number").text
-  position = doc.css("span.nba-player-header__position").text
+  player_2 = self.new
+  player_2.name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
+  player_2.jersey = doc.css("span.nba-player-header__jersey-number").text
+  player_2.position = doc.css("span.nba-player-header__position").text
+  player_2
 end
 
 def self.scrape_love
   doc = Nokogiri::HTML(open("http://www.nba.com/players/kevin/love/201567"))
-  name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
-  last_name = doc.css("p.nba-player-header__last-name").text.strip
-  jersey = doc.css("span.nba-player-header__jersey-number").text
-  position = doc.css("span.nba-player-header__position").text
+  player_3 = self.new
+  player_3.name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
+  player_3.jersey = doc.css("span.nba-player-header__jersey-number").text
+  player_3.position = doc.css("span.nba-player-header__position").text
+  player_3
 end
 
 end
