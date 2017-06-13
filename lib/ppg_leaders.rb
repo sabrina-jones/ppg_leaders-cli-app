d1fb624fd2cae04b232e541c82474a54cc174fe2
Sabrina Jones <sabrina.jones87@gmail.com>
2017-06-13 02:10:24 +0000

    added scrape_players method and scrape_james method

--- lib/ppg_leaders.rb
+++ lib/ppg_leaders.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 require_relative "ppg_leaders/version"
 require_relative './ppg_leaders/cli'
 require_relative './ppg_leaders/players'
--- lib/ppg_leaders/players.rb
+++ lib/ppg_leaders/players.rb
@@ -7,7 +7,6 @@ attr_accessor :name, :jersey, :position
     # 2. Kyrie Irving
     # 3. Kevin Love
   #  DOC
-
     player_1 = self.new
     player_1.name = "Lebron James"
     player_1.jersey = "23"
@@ -25,6 +24,28 @@ attr_accessor :name, :jersey, :position
 
     [player_1, player_2, player_3]
   end
+
+  def self.scrape_players
+  #go to cavs site, find players
+  #get player info
+  #instantiate the player
+  #return array of scraped players
+  players = []
+
+  players << self.scrape_james
+
+
+  players
+ end
+
+def self.scrape_james
+   doc = Nokogiri::HTML(open("http://www.nba.com/players/lebron/james/2544"))
+   name = doc.css("p.nba-player-header__first-name").text.strip
+   last_name = doc.css("p.nba-player-header__last-name").text.strip
+   jersey = doc.css("span.nba-player-header__jersey-number").text
+   position = doc.css("span.nba-player-header__position").text
+   binding.pry
+end
 
 
 end
