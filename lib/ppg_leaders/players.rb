fbaead2f2946dec0afd5c5537f56e5b272e85383
Sabrina Jones <sabrina.jones87@gmail.com>
2017-06-13 02:34:36 +0000

    added last name to name variable

--- lib/ppg_leaders/players.rb
+++ lib/ppg_leaders/players.rb
@@ -42,8 +42,7 @@ attr_accessor :name, :jersey, :position
 
 def self.scrape_james
    doc = Nokogiri::HTML(open("http://www.nba.com/players/lebron/james/2544"))
-   name = doc.css("p.nba-player-header__first-name").text.strip
-   last_name = doc.css("p.nba-player-header__last-name").text.strip
+   name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
    jersey = doc.css("span.nba-player-header__jersey-number").text
    position = doc.css("span.nba-player-header__position").text
    binding.pry
@@ -51,11 +50,9 @@ end
 
 def self.scrape_irving
   doc = Nokogiri::HTML(open("http://www.nba.com/players/kyrie/irving/202681"))
-  name = doc.css("p.nba-player-header__first-name").text.strip
-  last_name = doc.css("p.nba-player-header__last-name").text.strip
+  name = doc.css("p.nba-player-header__first-name").text.strip + " " + doc.css("p.nba-player-header__last-name").text.strip
   jersey = doc.css("span.nba-player-header__jersey-number").text
   position = doc.css("span.nba-player-header__position").text
-
 end
 
 
